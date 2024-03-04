26c550e2ac070d2f5c63e3ed4745586c9ec5e825
DarkArneus <asola268@gmail.com>
2024-03-04 18:18:21 +0000

    Puesto todo dentro de una clase y creado un main

--- puzzle1/puzzle1_1.rb
+++ puzzle1/puzzle1_1.rb
@@ -1,18 +1,26 @@
 require 'nfc'
 require 'colorize'
 
-# Create a new context
-ctx = NFC::Context.new
+class Rfid
+	def read_uid
+		# Create a new context
+		ctx = NFC::Context.new
 
-# Open the first available USB device
-dev = ctx.open nil
+		# Open the first available USB device
+		dev = ctx.open nil
 
-puts "Passa la teva targeta".green
+		puts "Passa la teva targeta".green
 
-# Imprimir la información de la tarjeta si se detecta
-loop do
-  tag_info = dev.select
-  puts "UID: #{tag_info}" unless tag_info == false
-  break if tag_info != false 
+		# Imprimir la información de la tarjeta si se detecta
+		loop do
+  			tag_info = dev.select
+ 			puts "UID: #{tag_info}" unless tag_info == false
+  			break if tag_info != false 
+		end
+	end
 end
-
+if __FILE__ == $0
+	rf = Rfid.new
+	uid = rf.read_uid
+	puts uid
+end
