3536783ea362d10674e4e8c15fca70f2755e5825
DarkArneus <asola268@gmail.com>
2024-03-14 14:58:33 +0000

    Ahora se devuelve el UID en el puzzle 1

--- puzzle1/puzzle1_1.rb
+++ puzzle1/puzzle1_1.rb
@@ -10,17 +10,17 @@ class Rfid
 		dev = ctx.open nil
 
 		puts "Passa la teva targeta".green
-
+		tag_info = nil
 		# Imprimir la información de la tarjeta si se detecta
 		loop do
   			tag_info = dev.select
- 			puts "UID: #{tag_info}" unless tag_info == false
-  			break if tag_info != false 
+ 			break if tag_info != false 
 		end
+	return tag_info
 	end
 end
 if __FILE__ == $0
 	rf = Rfid.new
 	uid = rf.read_uid
-	puts uid
+	puts "UID: #{uid}" unless uid.nil?
 end
