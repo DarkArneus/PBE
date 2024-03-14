11b0fe12bba811a79cc07e443544804b402b6474
DarkArneus <asola268@gmail.com>
2024-03-14 21:09:23 +0000

    He anadido 2 codigos de prueba, puzzle2_2.rb esta cerca de funcionar

--- puzzle2/puzzle2.rb
+++ puzzle2/puzzle2.rb
@@ -1,24 +1,26 @@
 require "gtk3"
 require_relative "../puzzle1/puzzle1_1.rb"
 
-# Creamos una ventana con el titulo RFID
-window = Gtk::Window.new("RFID")
-# Definimos la amplitud y longitud de la ventana
-window.set_size_request(400, 400)
-# Definimos la anchura de los bordes de la ventana
-window.set_border_width(10)
-
-# Creamos un nuevo Label 
-label = Gtk::Label.new("Please login with your university card")
-window.add(label)
-
+class ButtonWindow < Gtk::Window
+  def initialize
+    super
+        set_title 'RFID'
+        set_default_size 400,200
+        set_border_width 10
+        vbox = Gtk::Box.new(:vertical, 10)
+        add(vbox)
+        label = Gtk::Label.new("Please login with your university card\n")
+        vbox.pack_start(label)
+        button = Gtk::Button.new(label: 'Clear')
+        vbox.pack_start(button)
+end
+end
 rf = Rfid.new
 
 def handle_rfid_read(rf,label)
 	uid = rf.read_uid
 	label.set_text("UID: #{uid}")
 	Gtk.main_iteration while Gtk.events_pending?
-
 end
 
 def check_rfid(rf,label)
@@ -26,8 +28,8 @@ def check_rfid(rf,label)
 	return true
 end
 
-window.signal_connect("delete-event") { |_widget| Gtk.main_quit }
-window.show_all
+#window.signal_connect("delete-event") { |_widget| Gtk.main_quit }
+#window.show_all
 
 GLib::Timeout.add_seconds(1) do
 	check_rfid(rf,label)
