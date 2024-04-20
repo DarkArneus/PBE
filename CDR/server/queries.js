a32af5ff55b46ce97a27348016e8a4e659287c6f
DarkArneus <asola268@gmail.com>
2024-04-20 12:51:36 -0400

    Nova versió. (funcionalitats bàsiques)

--- CDR/server/queries.js
+++ CDR/server/queries.js
@@ -1,37 +1,83 @@
 const mysql = require('mysql');
+var url = require('url');
 const pool = mysql.createPool({
      host: '127.0.0.1', 
-     user: 'kappa', 
-     password: 'kappa',
-     database: 'cdr',
+     user: 'pbe', 
+     password: 'pbe',
+     database: 'db',
      port: 3306
 });
 
-function cercaEstudiant(uid, request, response){
+function writeResponse(sql, response) {
   pool.getConnection(function(err, connection) {
     if (err){
       console.error('Error al obtener la conexión: ', err)
       return;
     }
-    connection.query('SELECT name FROM students WHERE student_id= ?', [uid],function(error, results, fields) {
+
+    connection.query(sql,function(error, results, fields) {
       connection.release();
+      if (error){
+        response.writeHead(404, { "Content-Type": "application/json" });
+        response.write(JSON.stringify({ error: 'Error al ejecutar la consulta SQL:', err }));
+        response.end()
+        return;
+      }
+
       response.writeHead(200, {
         "Content-Type": "application/json",
       });
+
       response.write(
-        JSON.stringify({
-          name: results[0].name,
-          student_id: results[0].student_id
-        })
-      );
+        JSON.stringify(results));
       response.end();
     });
   }) 
 }
 
-function timetables(){
-  
+function cercaEstudiant(request, response){
+    sql = 'SELECT name FROM students WHERE student_id='+url.parse(request.url,true).query.student_id+';';
+    writeResponse(sql, response);
 }
 
-module.exports = { cercaEstudiant, timetables};
+function searchQuery(request, response) {
+  const reqURL = request.url;
+  // PARSE
+  var q = url.parse(reqURL, true); // parseamos la url
+  query = q.query;
+  param_keys = Object.keys(q.query); // retorna un array amb els parametres
+  param_values = Object.values(q.query); //retorna array amb els valors dels parametres
+  const keywordsOb={'[gt]': ' >', '[lt]':' <', '[gte]':' >=', '[lte]':' <='};
+  const reserved_key = ['limit'];
+  reserved = {}
+  var sql="";
+  //example.com/marks?subject=abc&name=123
+  console.log(query);
+  // FOR PER CAMBIAR: param1[gt] per param1 > per fer la query SQL
+  for (let clave in query) {
+    query[clave]='= ' + query[clave];
+    for (let keyword in keywordsOb) {
+      if (clave.includes(keyword)) {
+        old = clave
+        clave = clave.replace(keyword, keywordsOb[keyword]);
+        query[clave] = query[old].replace('= ','');
+        delete query[old];
+        console.log(clave);
+      }
+      console.log(query);
+    }
+  }
 
+// SELECT * FROM marks WHERE student_id = 12345678 AND mark < 9 ORDER BY asc AND subject = 'AST' LIMIT 4
+  
+
+    for (const clave in query) {
+      sql = sql + ` AND ${clave} ${query[clave]}`;
+      console.log(sql);
+    }
+
+    return sql+';';
+  //} 
+}
+
+module.exports = { cercaEstudiant, searchQuery, writeResponse};
--- CDR/server/server.js
+++ CDR/server/server.js
@@ -1,57 +1,29 @@
 const queries = require('./queries');
 var url = require('url');
 const http = require('http')
+var student_id;
 const server = http.createServer((request, response) => {
-    const reqURL = request.url;
     const reqMethod = request.method;
-    var student_uid;
-    
-    // PARSE
-    var q = url.parse(reqURL, true); // parseamos la url
-    data = q.query;
-    table = q.pathname; //  cogemos lo que hay antes de ?
-    param_keys = Object.keys(q.query); // retorna un array amb els parametres
-    param_values = Object.values(q.query); //retorna array amb els valors dels parametres
-    const keywords = ['[gt]', '[lt]','[gte]', '[lte]'];
-    const reserved_key = ['order', 'orderby', 'limit'];
-    const keywords_value = [' >','< ', ' >=', ' <='];
-    reserved_found =[];
-    reserved_values=[];
-    var sql;
-    //example.com/marks?subject=abc&name=123
+    const reqURL = request.url;
+    var q = url.parse(reqURL, true);
+    table = q.pathname.slice(1); //  cogemos lo que hay antes de ?
+                                 //example.com/marks?subject=abc&name=123
 
     switch (reqMethod) {
       default: {
-        defaultHandler(resquest, response)
+        defaultHandler(request, response);
       }
       case "GET": {
-        if(table === "/students") {
-          student_uid = data.student_uid;
-          console.log(data.student_id);
-          test = queries.cercaEstudiant(data.student_id, request, response);
-        }
-        if(param_keys.length){ //mirem que no tinguem una peticio buida sense parametres
-          sql = "SELECT * FROM ${table} WHERE student = ${student_uid}"
-          for (let i = 0; i <reserved_key.length; i++) {
-            if (param_keys.includes(reserved_key[i])) {
-              reserved_found.push(param_keys.splice(i ,1));
-              reserved_values.push(param_values.splice(i, 1));
-            }
-          }
-          for(let i = 0; i < param_keys.length; i++) {
-            for (let j = 0; j < keywords; j++) {
-              if(param_keys[i].includes(keywords[j]))
-                param_keys[i] = param_keys[i].replace(keywords[j], keywords_value[j]);
-              else
-                param_keys[i]=param_keys[i]+' =';
-           }
-          }
-        }
-        for(let i = 0; i < param_keys.length-1; i++) {
-          sql = sql + ' AND ${param_keys[i]} ${param_values[i]}';
+        if(table === "students") {
+          student_id = q.query.student_id;
+          console.log(student_id);
+          test = queries.cercaEstudiant(request, response);
+        }else{
+          sql = `SELECT * FROM ${table} WHERE uid = ${student_id}` + queries.searchQuery(request, response);
           console.log(sql);
-        } 
-     }
+          queries.writeResponse(sql, response);
+        }
+      }
     }
 });
  
