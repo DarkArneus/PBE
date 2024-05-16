48b0cf6011f08eb51eb0fb947f77f1003112d031
DarkArneus <asola268@gmail.com>
2024-05-16 10:55:51 -0400

    final

--- CDR/web-client/index.js
+++ CDR/web-client/index.js
@@ -25,15 +25,55 @@ function login(){
     xhr.send();
 }
 
-function search(){
-  var query = document.getElementById("search").value
+function search() {
+  var query = document.getElementById("search").value;
   var username = localStorage.getItem('username');
 
   var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) { // Quan la solicitut s'ha completat 
-      
-    }
+  xhr.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) { // Cuando la solicitud se ha completado 
+          var responseData = JSON.parse(this.responseText);
+          const titulo = Object.keys(responseData)[0];
+          if (responseData[titulo].length === 0) {
+              console.log("Query vacía");
+              return;
+          }
+
+          const headers = Object.keys(responseData[titulo][0]);
+          headers.pop();
+
+          const grid = document.getElementById("grid");
+          grid.innerHTML = "";
+
+          // Crear encabezados de tabla
+          const tr = document.createElement("tr");
+          headers.forEach((encabezado) => {
+              const th = document.createElement("th");
+              th.innerText = encabezado;
+              th.classList.add("table_headers");
+              tr.appendChild(th);
+          });
+          grid.appendChild(tr);
+
+          // Crear filas de tabla
+          responseData[titulo].forEach((item) => {
+              const tr = document.createElement("tr");
+              headers.forEach((header) => {
+                
+                if (header === "date"){
+                  item[header] = item[header].split("T")[0];
+                }
+                if(header === "day"){
+                  dayss = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+                  item[header] = dayss[item[header]-1];
+                }
+                const td = document.createElement("td");
+                td.innerText = item[header];
+                tr.appendChild(td);
+              });
+              grid.appendChild(tr);
+          });
+      }
   };
   var url = `http://localhost:9000/${query}`
   xhr.open("GET", url, true);
